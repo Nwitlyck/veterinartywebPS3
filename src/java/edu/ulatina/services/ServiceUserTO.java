02a9c5bfe1dd137635d3a6b5a16ab423392408a5
Nwitlyck <davidsegura118@hotmail.com>
2023-10-01 19:33:03 -0600

    Crud of Users

--- src/java/edu/ulatina/services/ServiceUserTO.java
+++ src/java/edu/ulatina/services/ServiceUserTO.java
@@ -2,6 +2,8 @@ package edu.ulatina.services;
 
 import edu.ulatina.objects.UserTO;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,17 +30,60 @@ public class ServiceUserTO extends Service implements ICrud<UserTO>{
 
     @Override
     public void update(UserTO objectTO) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+
+        ps = getConnection().prepareStatement("UPDATE Users SET Email = ?, Password = ?, Name = ?, Lastname = ?, Role = ?, State = ? WHERE Id = ?");
+        ps.setString(1, objectTO.getEmail());
+        ps.setString(2, objectTO.getPassword());
+        ps.setString(3, objectTO.getName());
+        ps.setString(4, objectTO.getLastname());
+        ps.setInt(5, objectTO.getRole());
+        ps.setInt(6, objectTO.getState());
+        ps.setInt(7, objectTO.getId());
+        ps.executeUpdate();
+
+        close(ps);
+        close(conn);
     }
 
     @Override
     public void delete(UserTO objectTO) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+
+        ps = getConnection().prepareStatement("UPDATE Users SET State = 0 WHERE Id = ?");
+        ps.setInt(1, objectTO.getId());
+        ps.executeUpdate();
+
+        close(ps);
+        close(conn);
     }
 
     @Override
     public List<UserTO> select() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<UserTO> objectTOList = new ArrayList<UserTO>();
+
+        ps = getConnection().prepareStatement("SELECT Id, Email, Password, Name, Lastname, Role, State FROM Users");
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            int id = rs.getInt("Id");
+            int email = rs.getInt("Email");
+            String password = rs.getString("Password");
+            String name = rs.getString("Name");
+            String lastname = rs.getString("Lastname");
+            int role = rs.getInt("Role");
+            int state = rs.getInt("State");
+
+            objectTOList.add(new UserTO(id, name, password, name, lastname, role, state));
+        }
+
+        close(rs);
+        close(ps);
+        close(conn);
+
+        return objectTOList;
     }
     
 }
