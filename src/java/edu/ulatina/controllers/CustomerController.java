3de141fcb41ab37b3d935cc9ee999769aa95f54d
vivigf <vivianagf26@hotmail.com>
2023-10-18 22:14:37 -0600

    np quiero escribir un mensaje bonito

--- src/java/edu/ulatina/controllers/CustomerController.java
+++ src/java/edu/ulatina/controllers/CustomerController.java
@@ -6,13 +6,14 @@
 package edu.ulatina.controllers;
 
 import edu.ulatina.objects.CustomersTO;
+import edu.ulatina.security.AESEncryptionDecryption;
 import edu.ulatina.services.ServiceCustomerTO;
 import java.io.Serializable;
 import java.util.*;
+import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.*;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
@@ -59,6 +60,7 @@ public class CustomerController implements Serializable{
      @PostConstruct
      public void inicializate(){
          serviceCustomerTO = new ServiceCustomerTO();
+         
      }
      
     public List<CustomersTO> getCustomerList() {
@@ -82,5 +84,122 @@ public class CustomerController implements Serializable{
         }
         return returnList;
     }
+      private boolean notNull() {
+
+          if (selectedCustomerTO.getCedula() == 0) {
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_WARN, "Valor Nulo", "La cédula está vacía"));
+            return false;
+        }
+          if (selectedCustomerTO.getName().isEmpty() || selectedCustomerTO.getName() == null) {
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_WARN, "Valor Nulo", "El nombre esta vacio"));
+            return false;
+        }
+        if (selectedCustomerTO.getLastname().isEmpty() || selectedCustomerTO.getLastname() == null) {
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_WARN, "Valor Nulo", "El apellido esta vacio"));
+            return false;
+        }
+        if (selectedCustomerTO.getEmail().isEmpty() || selectedCustomerTO.getEmail() == null) {
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_WARN, "Valor Nulo", "El correo esta vacio"));
+            return false;
+        }
+
+        return true;
+    }
+      private boolean followMetrics() {
+
+        String regexPatternEmail = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+        if (!Pattern.compile(regexPatternEmail).matcher(this.selectedCustomerTO.getEmail()).matches()) {
+
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalido", "El correo ingresado no es valido"));
+            return false;
+        }
+
+        for (CustomersTO customer : getCustomerList()) {
+            if (selectedCustomerTO.getEmail() == customer.getEmail()) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalido", "El correo ya existe"));
+                return false;
+            }
+        }
+        
+        for (CustomersTO customer : getDisableCustomerList()) {
+            if (selectedCustomerTO.getEmail() == customer.getEmail()) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Invalido", "El correo ya existe"));
+                return false;
+            }
+        }
+        return true;
+    }
     
+    public void insertCustomer() {
+
+        if (!notNull()) {
+            return;
+        }
+
+        if (!followMetrics()) {
+            return;
+        }
+
+        selectedCustomerTO.setCedula(selectedCustomerTO.getCedula());
+
+        try {
+            this.serviceCustomerTO.insert(selectedCustomerTO);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error al hacer insert en base de datos"));
+        } finally {
+            selectedCustomerTO = new CustomersTO();
+        }
+    }
+    
+    public void updateCustomer() {
+        
+            for(CustomersTO customer : getCustomerList()){
+                if(customer.getCedula()== selectedCustomerTO.getCedula()){
+                    // selectedCustomerTO.setPassword(new AESEncryptionDecryption().decrypt(customer.getPassword()));
+                }
+            }
+  
+        
+        if (!notNull()) {
+            return;
+        }
+
+        if (!followMetrics()) {
+            return;
+        }
+
+        selectedCustomerTO.setCedula(selectedCustomerTO.getCedula());
+
+        try {
+            this.serviceCustomerTO.update(selectedCustomerTO);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error al hacer update en base de datos"));
+        } finally {
+            selectedCustomerTO = new CustomersTO();
+        }
+    }
+    
+     public void disableCustomer() {
+        try {
+            serviceCustomerTO.delete(selectedCustomerTO);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error al desabilitar el usuario en base de datos"));
+        }
+        this.selectedCustomerTO = new CustomersTO();
+
+    }
+
+    public void enableCustomer() {
+        try {
+            serviceCustomerTO.enable(selectedCustomerTO);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_ERROR, "ERROR", "Error al desabilitar el usuario en base de datos"));
+        }
+        this.selectedCustomerTO = new CustomersTO();
+    }
 }
