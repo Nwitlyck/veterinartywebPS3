1752f3b3938fd9951988d8db80c8b74938f98fa7
Nwitlyck <davidsegura118@hotmail.com>
2023-10-26 16:06:53 -0600

    Fix command buttons not having update to their dialog

--- src/java/edu/ulatina/controllers/SiteController.java
+++ src/java/edu/ulatina/controllers/SiteController.java
@@ -45,6 +45,7 @@ public class SiteController implements Serializable {
 
     public void setSelectedSite(SiteTO selectedSite) {
         this.selectedSite = selectedSite;
+        fillMapCanton();
     }
 
     public Map<String, Integer> getMapProvince() {
@@ -182,6 +183,7 @@ public class SiteController implements Serializable {
     
     public void resetSelectedSite(){
         this.selectedSite = new SiteTO();
+        fillMapCanton();
     }
     
     @PostConstruct
